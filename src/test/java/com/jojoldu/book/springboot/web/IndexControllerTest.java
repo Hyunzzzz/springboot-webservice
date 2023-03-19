5af33966c2aabb58345ba34cef3bafd5ff9b5462
Hyunzzzz <simonha9721@gmail.com>
2023-03-19 21:55:00 +0900

    화면 구현

--- src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -1,4 +1,4 @@
-package com.jojoldu.book.springboot.web.dto;
+package com.jojoldu.book.springboot.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,17 +12,14 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-public class IndexControllerTest {
-
+public class IndexControllerTest{
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
     public void 메인페이지_로딩(){
-        //when
         String body = this.restTemplate.getForObject("/",String.class);
 
-        //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        assertThat(body).contains("hi");
     }
 }
